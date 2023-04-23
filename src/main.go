0c7890b0cad5e4da47d6b8ba4ebc9a8a614c6d10
leath-dub <77889270+leath-dub@users.noreply.github.com>
2023-04-23 13:56:56 +0100

    added all methods to be allowed

--- src/main.go
+++ src/main.go
@@ -208,6 +208,7 @@ func CORSMiddleware() gin.HandlerFunc {
     return func(c *gin.Context) {
         c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
         c.Writer.Header().Set("Access-Control-Allow-Headers", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "*")
 
         if c.Request.Method == "OPTIONS" {
             c.AbortWithStatus(204)
